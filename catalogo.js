6f9d2c07292b15ca104b6348ed13f1a5e2f03476
German Picas <german.g.picas@gmail.com>
2022-12-29 20:29:37 -0300

    Primer archivo del repositorio

---
+++ catalogo.js
@@ -0,0 +1,66 @@
+const comidas = [
+    {
+        id: 1,
+        nombre: 'Pizza',
+        estilo: 'Margarita',
+        imagen: 'img/Margarita.jpeg',
+        valor: 1000,
+        cantidad: 1
+    },
+    {
+        id: 2,
+        nombre: 'Pizza',
+        estilo: 'Napolitana',
+        imagen: 'img/Napolitana.jpg',
+        precio: 1200,
+        cantidad: 1
+    },
+    {
+        id: 3,
+        nombre: 'Pizza',
+        estilo: 'Primavera',
+        imagen: 'img/primavera.jpg',
+        precio: 1200,
+        cantidad: 1
+    },
+    {
+        id: 4,
+        nombre: 'Pizza',
+        estilo: 'Calabresa',
+        imagen: 'img/calabresa.jpg',
+        precio: 1200,
+        cantidad: 1
+    },
+    {
+        id: 5,
+        nombre: 'Paty',
+        estilo: 'Cheesburguer',
+        imagen: 'img/Cheesburguer.jpg',
+        precio: 1000,
+        cantidad: 1
+    },
+    {
+        id: 6,
+        nombre: 'Paty',
+        estilo: 'Argenta', 
+        imagen: 'img/argenta.jpg',
+        precio: 1200,
+        cantidad: 1
+    },
+    {
+        id: 7,
+        nombre: 'Paty',
+        estilo: 'Hamburguesa con Bacon',
+        imagen: 'img/HamburguesaBacon.jpg',
+        precio: 1200,
+        cantidad: 1
+    },
+    {
+        id: 8,
+        nombre: 'Paty',
+        estilo: 'Vegana',
+        imagen: 'img/vegana.jpg',
+        precio: 1000,
+        cantidad: 1
+    },
+]
---
+++ main.js
@@ -0,0 +1,33 @@
+const resultado = document.getElementById("resultado");
+
+const formulario = document.getElementById("formulario");
+
+const filtrar = () => {
+    
+    resultado.innerHTML = '';
+
+    const texto = formulario.value.toLowerCase();
+    for (let comida of comidas){
+        let nombre = comida.nombre.toLowerCase();
+
+        if (nombre.indexOf(texto) !== -1) {
+            resultado.innerHTML += `
+            <div class="card" style="width: 18rem;" id="resultado">
+                <img src=${comida.imagen} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre: ${comida.nombre}</h5>
+                    <h5 class="card-title">Estilo: ${comida.estilo}</h5>
+                    <p class="card-text">Valor: ${comida.precio}</p>
+                    <a class="btn btn-danger" href="carrito.html" >Agregar al carrito</a> 
+                </div>
+            </div>
+            `
+        }
+    }
+    if (resultado.innerHTML === ''){
+        resultado.innerHTML = `<li>Producto no encontrado</li>`
+    }
+}
+
+formulario.addEventListener('keyup', filtrar)
+filtrar();
